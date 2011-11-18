bb1b4be1f311fb4d489e619ec9cf5e6248cfd334
Luca Pinello <lucapinello@gmail.com>
2011-11-18 15:51:57 -0500

    added tmpfile module to fimo

--- bioutilities.py
+++ bioutilities.py
@@ -535,8 +535,7 @@ class Fimo:
             fimo_process=subprocess.Popen(self.fimo_command+' '+tmp_filename,stdin=None,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
             output=fimo_process.communicate()[0]
             fimo_process.wait()
-            fimo
-            
+                       
             print output
             os.remove(tmp_filename)
             
