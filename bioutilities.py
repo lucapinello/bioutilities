f15c708fcfa5d5ddbf298e0f74a0f3bda4c29193
Luca Pinello <lpinello@.bcb.local>
2013-03-25 18:41:21 -0400

    fix strand information on gene parsing

--- bioutilities.py
+++ bioutilities.py
@@ -427,26 +427,47 @@ class Gene:
             return Coordinate(self.c.chr_id,self.end-self.regions[3],self.tss+self.regions[0],strand='-') 
         
     @classmethod    
-    def load_from_annotation(cls,gene_annotation_file,load_exons_introns_info=False,header_lines=1,regions=regions):
+    def load_from_annotation(cls,gene_annotation_file,load_exons_introns_info=False,header_lines=1,regions=regions,format='txt'):
         genes_list=[]
         with open(gene_annotation_file,'r') as genes_file:
             
             for _ in range(header_lines):
                 genes_file.readline()
            
+            if format=='txt':
+                idx_chr_id=2
+                idx_bpstart=4
+                idx_bpend=5
+                idx_strand=3
+                idx_access=1
+                idx_name=-4
+                idx_exon_starts=9
+                idx_exon_ends=10
+            else:
+               idx_chr_id=0
+               idx_bpstart=1
+               idx_bpend=2
+               idx_strand=5
+               idx_access=3
+               idx_name=3
+               idx_exon_starts=10
+               idx_exon_ends=11
+               
+               
+           
             for gene_line in genes_file:
                 fields=gene_line.split('\t')
-                chr_id=fields[2]
-                bpstart=int(fields[4])
-                bpend=int(fields[5])
-                strand=fields[3]
-                access=fields[1]
-                name=fields[-4]
+                chr_id=fields[idx_chr_id]
+                bpstart=int(fields[idx_bpstart])
+                bpend=int(fields[idx_bpend])
+                strand=fields[idx_strand]
+                access=fields[idx_access]
+                name=fields[idx_name]
                 c=Coordinate(chr_id,bpstart,bpend,strand=strand)
 
                 if load_exons_introns_info:
-                    exon_starts=map(int,fields[9].split(',')[:-1])
-                    exon_ends=map(int,fields[10].split(',')[:-1])
+                    exon_starts=map(int,fields[idx_exon_starts].split(',')[:-1])
+                    exon_ends=map(int,fields[idx_exon_ends].split(',')[:-1])
 
                     exons=[Coordinate(chr_id,bpstart,bpend,strand=strand) for bpstart,bpend in zip(exon_starts,exon_ends)]
                     introns=[Coordinate(chr_id,bpstart+1,bpend-1,strand=strand) for bpstart,bpend in zip(exon_ends[:-1],exon_starts[1:])]
@@ -528,7 +549,7 @@ class Gene:
         if promoter_region:
             return [g.promoter_c for g in cls.load_from_annotation(gene_annotation_file,load_exons_introns_info=False,header_lines=1,regions=[Gene.regions[0],promoter_region[0],promoter_region[1],Gene.regions[3]])]
         else:
-             return [g.promoter_c for g in cls.load_from_annotation(gene_annotation_file,load_exons_introns_info=False,header_lines=1)]
+            return [g.promoter_c for g in cls.load_from_annotation(gene_annotation_file,load_exons_introns_info=False,header_lines=1)]
             
     
     @classmethod
