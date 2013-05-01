5c720b5c529c3157d716ffb685f593a5e135e55a
Luca Pinello <lpinello@.bcb.local>
2013-05-01 16:31:15 -0400

    update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='bioutilities',
-      version='0.1',
+      version='0.2',
       py_modules=['bioutilities'],
+      description='A collection of tools for biological computation',
+      install_requires=['numpy','scipy','xlrd','xlwt','blist','pysam'],
       )
